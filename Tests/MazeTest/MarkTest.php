758bf74a771ab9bb64de933759750c27c0661092
Qingshan Luo <shanshan.lqs@gmail.com>
2017-05-02 13:26:05 +0800

    Use PHPUnit

---
+++ Tests/MazeTest/MarkTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *+------------------------------------------------------------------------------------------------+
+ *| A PHP Configuration File Manager                                                               |
+ *+------------------------------------------------------------------------------------------------+
+ *| @license   Apache License 2.0                                                                  |
+ *| @copyright Copyright (c) 2017 Qingshan Luo                                                     |
+ *+------------------------------------------------------------------------------------------------+
+ *| @author    Qingshan Luo <shanshan.lqs@gmail.com>                                               |
+ *+------------------------------------------------------------------------------------------------+
+ */
+namespace MazeTest;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is an example.
+ */
+class MarkTest extends TestCase
+{
+    public function testExample()
+    {
+
+        $this->assertTrue(true);
+    }
+}
